4a4718f62da2d61f43cff9f0e1b47b80f4b50843
Adil Aldamzharov <a.aldamzharov@bytepride.dev>
2021-12-22 20:18:27 +0600

    feat(store): Added profile component. Fixed nav

--- projects/store/src/app/app-routing.module.ts
+++ projects/store/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MainComponent} from "./main/main.component";
 import {LibraryComponent} from "./library/library.component";
+import {ProfileComponent} from "./profile/profile.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/main', pathMatch: 'full'},
   {path: 'main', component: MainComponent},
   {path: 'library', component: LibraryComponent},
+  {path: 'profile', component: ProfileComponent},
 ];
 
 @NgModule({
--- projects/store/src/app/app.module.ts
+++ projects/store/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { LibraryComponent } from './library/library.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    LibraryComponent
+    LibraryComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
