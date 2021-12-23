6ebde1d72894798868f1cbeab8a49aef5660c95f
Adil Aldamzharov <a.aldamzharov@bytepride.dev>
2021-12-24 01:44:22 +0600

    feat(store): Added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
   },
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        spin: 'spin 1s linear infinite'
+      }
+    },
   },
   plugins: [],
 }
