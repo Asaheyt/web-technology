9bceee5ba39715f5fcb58404289723e247815ec6
Adil Aldamzharov <a.aldamzharov@bytepride.dev>
2021-12-23 23:13:27 +0600

    feat(store): Added about page

--- projects/store/src/app/app-routing.module.ts
+++ projects/store/src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'item', component: ItemComponent},
+  {path: 'about', loadChildren: () => import('../app/about/about.module').then(m => m.AboutModule)}
 ];
 
 @NgModule({
--- projects/store/src/app/app.component.ts
+++ projects/store/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'store';
-  auth = false;
+  auth = window.localStorage.getItem("email");
 }
