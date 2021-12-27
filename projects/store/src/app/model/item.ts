0e88bdcba2976e22b1edd09f408f97aab2a19666
Adil Aldamzharov <a.aldamzharov@bytepride.dev>
2021-12-27 22:16:22 +0600

    feat(store): Added comments

--- projects/store/src/app/model/item.ts
+++ projects/store/src/app/model/item.ts
@@ -1,7 +1,10 @@
+import {Comment} from "./comment";
+
 export interface Item {
   id: number,
   title: string,
   description: string,
   owners: Array<string>,
   img: string,
+  comments: Array<Comment>
 }
